889e25e0767f93d0535ead859d7116eea385224c
keith66fuller <keith66fuller@gmail.com>
2018-06-29 16:23:43 -0500

    skeleton

---
+++ public/js/user-setup.js
@@ -0,0 +1,124 @@
+$(document).ready(function () {
+    Storage.prototype.setObj = function(key, obj) {
+        return this.setItem(key, JSON.stringify(obj))
+    }
+    Storage.prototype.getObj = function(key) {
+        return JSON.parse(this.getItem(key))
+    }
+    // SHOW NEWS SOURCES ON PAGE LOAD
+    // =========================================
+
+    // Getting the initial list of sources
+    getSources();
+
+    // Function for retrieving list of news sources and getting them ready to be rendered to the page
+    function getSources() {
+        $.get("/api/sources", function (data) {
+            $(".news-sources-list").empty();
+            var rowsToAdd = [];
+            for (var i = 0; i < data.length; i++) {
+                rowsToAdd.push(createSourcesRow(data[i]));
+            }
+        });
+    }
+
+    // Function for creating a new list row for each news source
+    function createSourcesRow(sourcesData) {
+
+        // console.log(sourcesData);
+
+        var newDiv = $("<div>");
+        var $ctrl = $('<input/>').attr({ type: 'checkbox', id: sourcesData.id, value: sourcesData.id, name: sourcesData.name }).addClass("Checkbox");
+        $(newDiv).append($ctrl);
+        $(newDiv).append("<label for=" + sourcesData.id + ">" + sourcesData.name + "</label>");
+        $(".news-sources-list").append(newDiv);
+
+        // addSelectAll();
+    }
+
+    function addSelectAll() {
+        // add a 'select all' checkbox
+        $(newDiv).append($ctrl);
+        $(newDiv).append("<label>" + "Select All" + "</label>");
+        $(".news-sources-list").prepend(selectAllCheckbox);
+    }
+
+    // $("#checkedAll").change(function () {
+    //     if (this.checked) {
+    //         $(".checkSingle").each(function () {
+    //             this.checked = true;
+    //         });
+    //     } else {
+    //         $(".checkSingle").each(function () {
+    //             this.checked = false;
+    //         });
+    //     }
+    // });
+
+    // $(".checkSingle").click(function () {
+    //     if ($(this).is(":checked")) {
+    //         var isAllChecked = 0;
+
+    //         $(".checkSingle").each(function () {
+    //             if (!this.checked)
+    //                 isAllChecked = 1;
+    //         });
+
+    //         if (isAllChecked == 0) {
+    //             $("#checkedAll").prop("checked", true);
+    //         }
+    //     }
+    //     else {
+    //         $("#checkedAll").prop("checked", false);
+    //     }
+    // });
+
+    // MAIN PROCESS WHEN SUBMIT BUTTON CLICKED
+    // =========================================
+    // $(".user-setup-submit").on("click", function () {
+    //     // check to see which checkboxes were selected by the user
+    //     // grab this info and create as a string/array
+    //     var checklistString = $('.Checkbox:checked').map(function () {
+    //         return this.value;
+    //     }).get().join(',')
+    //     console.log(checklistString);
+    //     // enter these news sources into user table as a string
+    //     // redirect user to main.html
+    //     // $(location).attr('href', '/main.html')
+    // });
+
+    $("#checkAll").click(function () {
+        $(".Checkbox").prop('checked', $(this).prop('checked'));
+    });
+
+    $("#exampleModal").on("click", function () {
+        // check to see which checkboxes were selected by the user
+        // grab this info and create as a string/array
+        var emptyArray = [];
+        var checklistString = $('.Checkbox:checked').map(function () {
+            return this.value;
+        }).get()
+
+        console.log("CHECKLISTSTRING: "+checklistString);
+
+        checklistString.forEach((e,i) => {
+            console.log("CLS: "+i+" "+e)
+        });
+
+        var newUser = {
+            uid: localStorage.getObj("uid"),
+            username: $("#userName").val(),
+            firstName: $("#firstName").val(),
+            lastName: $("#lastName").val(),
+            email: localStorage.getObj("email"),
+            sources: checklistString
+        }
+
+        console.log("NEWUSER: "+JSON.stringify(newUser, null,2));
+
+        $.post("/api/user", newUser)
+            .then(console.log("user created"));
+
+        $(location).attr('href', '/main.html')
+    });
+});
---
+++ server.js
@@ -0,0 +1,57 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// ============================================================= 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static("public"));
+app.use('/bower_components',express.static("bower_components"));
+
+express.static.mime.define({'text/javascript': ['js']});
+
+
+
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/source-api-routes.js")(app);
+require("./routes/article-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/source-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+
+
+
+
